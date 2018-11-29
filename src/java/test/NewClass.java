572f8e28247c150bffcca9d64d3009b8a14d6b75
Elkin Madrid <emadrid@Civitrans.com>
2018-11-29 18:55:16 -0500

    commit inicial

---
+++ src/java/test/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author emadrid
+ */
+public class NewClass {
+
+    /**
+     * @return the i
+     */
+    public int getI() {
+        return i;
+    }
+
+    /**
+     * @param i the i to set
+     */
+    public void setI(int i) {
+        this.i = i;
+    }
+    
+    private int i;
+    
+}
